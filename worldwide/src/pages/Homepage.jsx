81dcfcaddb0f8818064bd67274a02a5393640351
Eason_chiang <95297596+class83108@users.noreply.github.com>
2025-02-19 09:43:41 +0800

    新增 React Router 支援，建立基本頁面結構與導航

---
+++ worldwide/src/pages/Homepage.jsx
@@ -0,0 +1,13 @@
+import PageNav from "../components/PageNav";
+
+function Homepage() {
+  return (
+    <div>
+      <PageNav />
+      <h1>WorldWide</h1>
+    </div>
+  );
+}
+
+export default Homepage;
+Homepage;
