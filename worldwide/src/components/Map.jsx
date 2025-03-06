b66f974b7a10c47551a67e222d60cfb930c179b2
Eason_chiang <95297596+class83108@users.noreply.github.com>
2025-03-06 14:32:38 +0800

    新增 useUrlPosition Hook 以從 URL 獲取地理位置，並在地圖和表單元件中整合使用；新增 createCity 方法以支持城市創建功能，並更新相關元件以處理城市資料

--- worldwide/src/components/Map.jsx
+++ worldwide/src/components/Map.jsx
@@ -1,4 +1,4 @@
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import {
   MapContainer,
   TileLayer,
@@ -13,11 +13,11 @@ import styles from "./Map.module.css";
 import { useEffect, useState } from "react";
 import { useCities } from "../contexts/CitiesContext";
 import { useGeolocation } from "../hooks/useGeolocation";
+import { useUrlPosition } from "../hooks/useUrlPosition";
 
 function Map() {
   const { cities } = useCities();
   const [mapPosition, setMapPosition] = useState([40, 0]);
-  const [searchParams] = useSearchParams();
 
   const {
     isLoading: isLoadingPosition,
@@ -25,8 +25,7 @@ function Map() {
     getPosition,
   } = useGeolocation();
 
-  const mapLat = searchParams.get("lat");
-  const mapLng = searchParams.get("lng");
+  const [mapLat, mapLng] = useUrlPosition();
 
   useEffect(
     function () {
